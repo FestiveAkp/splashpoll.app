9f1efac75095e31d3c034c0422277984e353b128
Akash Pillai <akashp807@gmail.com>
2021-01-06 22:52:11 -0600

    Add background wave and SplashContainer styling

--- src/components/SplashUtils.js
+++ src/components/SplashUtils.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Wave from 'react-wavify';
 import {
+    Box,
     Center,
     Container,
     Heading
@@ -7,17 +9,45 @@ import {
 
 export const SplashHeader = () => (
     <>
-        <Heading as="h1">Poll App</Heading>
+        <Heading as="h1" fontStyle="italic" lineHeight="normal">Splashpoll.app</Heading>
         <Heading size="sm" fontWeight="semibold" as="h2" mt={2} mb={10} >Instantly create online polls with open-ended responses</Heading>
     </>
 );
 
 export const SplashContainer = props => (
-    <>
-        <Center minHeight="100vh">
-            <Container pt={4} pb={45}>
+    <Box position="relative">
+        <Center className="chakra-center" minHeight="100vh" zIndex="5">
+            <Container
+                pt={4}
+                pb={5}
+                mb={4}
+                backgroundColor="white"
+                borderWidth="1px"
+                borderColor="gray.200"
+                borderRadius="md"
+                shadow="sm"
+            >
                 {props.children}
             </Container>
         </Center>
-    </>
+        <Box
+            position="absolute"
+            left="0"
+            right="0"
+            bottom="0"
+            zIndex="-1"
+        >
+            <Wave
+                fill='#1DA1F2'
+                paused={false}
+                options={{
+                    height: 20,
+                    amplitude: 30,
+                    speed: 0.25,
+                    points: 3
+                }}
+                style={{ display: 'block' }}
+            />
+        </Box>
+    </Box>
 );
