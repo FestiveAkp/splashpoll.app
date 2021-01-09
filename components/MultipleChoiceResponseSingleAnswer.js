00004b655e68eb06385ff3d72810687e1a9e582e
Akash Pillai <akashp807@gmail.com>
2021-01-09 13:49:29 -0600

    Fix radio-group selection bug

--- components/MultipleChoiceResponseSingleAnswer.js
+++ components/MultipleChoiceResponseSingleAnswer.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Box, Stack, RadioGroup, Radio } from '@chakra-ui/react';
+import { Box, Stack, Radio } from '@chakra-ui/react';
 
 export default function MultipleChoiceResponseSingleAnswer(props) {
     const { options, choice, update } = props;
 
     return (
         <Box as="section" mt={10} mb={8}>
-            <RadioGroup onChange={value => update(value)} value={choice}>
                 <Stack spacing={3}>
                     {options.map((option, i) => (
-                        <Radio key={i} value={option} spacing="1rem" size="lg">{option}</Radio>
+                        <Radio
+                            key={i}
+                            value={option}
+                            isChecked={choice === option}
+                            onChange={e => update(e.target.value)}
+                            spacing="1rem"
+                            size="lg"
+                        >
+                            {option}
+                        </Radio>
                     ))}
                 </Stack>
-            </RadioGroup>
         </Box>
     );
 }
