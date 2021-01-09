a01cee9ffb5762b3729e57832461728527fecef3
Akash Pillai <akashp807@gmail.com>
2021-01-09 15:22:11 -0600

    Handle toast margin using ChakraProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { SplashHeader, SplashContainer } from '../components/SplashUtils';
-import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
+    const theme = extendTheme({
+        styles: {
+            global: {
+                '.chakra-toast__inner': {
+                    marginTop: '3rem !important'
+                }
+            }
+        }
+    });
+
     return (
         <>
-            <ChakraProvider>
+            <ChakraProvider theme={theme}>
                 <SplashContainer>
                     <SplashHeader />
                     <Component {...pageProps} />
