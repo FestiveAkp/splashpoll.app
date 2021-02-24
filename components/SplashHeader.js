4f74ee369ccb30e78d68fcb98b62663ff6519a7c
Akash Pillai <akashp807@gmail.com>
2021-02-23 23:00:50 -0600

    Unitalicize site title

--- components/SplashHeader.js
+++ components/SplashHeader.js
@@ -5,9 +5,9 @@ import { Box, Heading, Tag } from '@chakra-ui/react';
 const SplashHeader = () => (
     <Box as="header" mb={10}>
         <Link href="/">
-            <Heading fontStyle="italic" lineHeight="normal" cursor="pointer">
+            <Heading lineHeight="normal" cursor="pointer">
                 Splashpoll.app
-                <Tag colorScheme="pink" ml={2}>Beta</Tag>
+                <Tag fontStyle="italic" colorScheme="pink" ml={2}>Beta</Tag>
             </Heading>
         </Link>
         <Heading as="h2" size="sm" fontWeight="semibold" mt={2}>Instantly create online polls with open-ended responses</Heading>
